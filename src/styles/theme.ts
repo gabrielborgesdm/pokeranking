927dd0e814ac4983724dddea0e55105a55607920
Gabriel Borges <gabrielborgesdemoraes@gmail.com>
2021-08-01 16:25:38 -0300

    [ADDED] Translation package added

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,8 +1,16 @@
+const colors = {
+  blue: '#028090',
+  red: '#F45B69',
+  yellow: '#FFE74C',
+  white: '#FFFFFF',
+  black: '#474044'
+}
+
 const theme = {
   colors: {
-    background: '#121214',
-    text: '#e1e1e6',
-    primary: '#8257e6'
+    background: colors.blue,
+    text: colors.white,
+    primary: colors.red
   }
 }
 
