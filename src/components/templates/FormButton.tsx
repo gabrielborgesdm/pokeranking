58e69e451e80e02c630ed9e27af6ab26e7934270
Gabriel Borges <gabrielborgesdemoraes@gmail.com>
2021-08-02 23:10:18 -0300

    [ADDED] Form Button template

---
+++ src/components/templates/FormButton.tsx
@@ -0,0 +1,31 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import useTranslation from 'next-translate/useTranslation'
+import React from 'react'
+import { faSpinner } from '@fortawesome/fontawesome-free-solid'
+import { BlueButton } from '../../styles/common'
+
+interface IFormButton {
+  title: string,
+  type?: string,
+  isLoading?: boolean,
+  isDisabled?: boolean,
+  onClick?: Function,
+}
+
+const FormButton: React.FC<IFormButton> = ({ title, type = 'submit', onClick, isLoading, isDisabled }: IFormButton) => {
+  const { t } = useTranslation('common')
+  return (
+    <BlueButton variant="secondary" type={type} onClick={onClick} disabled={isLoading || isDisabled}>
+      {isLoading
+        ? (
+        <>
+          <FontAwesomeIcon icon={faSpinner} spin />&nbsp;
+          {t('loading')}
+        </>
+          )
+        : title}
+    </BlueButton>
+  )
+}
+
+export default FormButton
