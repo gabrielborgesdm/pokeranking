e4e94e2681496c2ebc49f7ea3f8c3f74f8182f65
Gabriel Borges <gabrielborgesdemoraes@gmail.com>
2021-09-14 23:28:51 -0300

    Add sentry to the app

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
-const withImages = require('next-images')
+const withPlugins = require('next-compose-plugins')
 const nextTranslate = require('next-translate')
+const { withSentryConfig } = require('@sentry/nextjs')
 
-module.exports = nextTranslate({
+const nextConfig = {
   esModule: true,
   reactStrictMode: true,
   typescript: {
     ignoreBuildErrors: true
   }
-})
+}
+
+module.exports = withPlugins([nextTranslate, withSentryConfig], nextConfig)
