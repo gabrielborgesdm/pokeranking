31829c15539e681df6910e44dd2e6e2e0062fb9b
Gabriel Borges <gabrielborgesdemoraes@gmail.com>
2021-09-15 19:23:44 -0300

    Enhance design of modals and buttons

--- src/components/PokemonsSelectList.tsx
+++ src/components/PokemonsSelectList.tsx
@@ -2,16 +2,24 @@ import useTranslation from 'next-translate/useTranslation'
 import React, { useEffect, useState } from 'react'
 import { Dropdown, Form, FormControl } from 'react-bootstrap'
 import { REQUEST_URL } from '../configs/AppConfig'
-import { IPokemon, IPokemonsResponse, IPokemonType } from '../configs/types/IPokemon'
+import {
+  IPokemon,
+  IPokemonsResponse,
+  IPokemonType
+} from '../configs/types/IPokemon'
 import { useFetch } from '../services/FetchService'
 
 interface IPokemonsSelectList {
-  userPokemons: Array<IPokemon>;
-  selectedPokemon?: IPokemonType;
-  setSelectedPokemon: (pokemon: IPokemonType) => void;
+  userPokemons: Array<IPokemon>
+  selectedPokemon?: IPokemonType
+  setSelectedPokemon: (pokemon: IPokemonType) => void
 }
 
-export const PokemonsSelectList: React.FC<IPokemonsSelectList> = ({ userPokemons, selectedPokemon, setSelectedPokemon }: IPokemonsSelectList) => {
+export const PokemonsSelectList: React.FC<IPokemonsSelectList> = ({
+  userPokemons,
+  selectedPokemon,
+  setSelectedPokemon
+}: IPokemonsSelectList) => {
   const { data } = useFetch<IPokemonsResponse>(REQUEST_URL.POKEMONS)
 
   const { t } = useTranslation('pokemons')
@@ -22,18 +30,25 @@ export const PokemonsSelectList: React.FC<IPokemonsSelectList> = ({ userPokemons
   useEffect(() => {
     if (data?.pokemons) {
       const idsOfUserPokemons = userPokemons.map(pokemon => pokemon.id)
-      const newPokemonsList = data.pokemons.filter(pokemon => !idsOfUserPokemons.includes(pokemon.id))
+      const newPokemonsList = data.pokemons.filter(
+        pokemon => !idsOfUserPokemons.includes(pokemon.id)
+      )
       setPokemonsList(newPokemonsList)
     }
   }, [data])
 
   const renderCustomToggle = ({ children, onClick }, ref) => {
-    const handleClick = (e) => {
+    const handleClick = e => {
       e.preventDefault()
       onClick(e)
     }
     return (
-      <Form.Select ref={ref} onClick={handleClick} onMouseDown={(e) => e.preventDefault()}>
+      <Form.Select
+        className="cursor-pointer"
+        ref={ref}
+        onClick={handleClick}
+        onMouseDown={e => e.preventDefault()}
+      >
         <option>{children}</option>
       </Form.Select>
     )
@@ -41,7 +56,10 @@ export const PokemonsSelectList: React.FC<IPokemonsSelectList> = ({ userPokemons
 
   const CustomToggle = React.forwardRef(renderCustomToggle)
 
-  const renderCustomMenu = ({ children, style, className, 'aria-labelledby': labeledBy }, ref) => {
+  const renderCustomMenu = (
+    { children, style, className, 'aria-labelledby': labeledBy },
+    ref
+  ) => {
     return (
       <div
         ref={ref}
@@ -53,16 +71,19 @@ export const PokemonsSelectList: React.FC<IPokemonsSelectList> = ({ userPokemons
           autoFocus
           className="mx-3 my-2 w-auto"
           placeholder={t('search-pokemon')}
-          onChange={(e) => setPokemonFilter(e.target.value)}
+          onChange={e => setPokemonFilter(e.target.value)}
           value={pokemonFilter}
         />
-        <ul className="list-unstyled" style={{ maxHeight: '300px', overflowY: 'auto' }}>
-          {React.Children.toArray(children).length > 0 && (
+        <ul
+          className="list-unstyled"
+          style={{ maxHeight: '300px', overflowY: 'auto' }}
+        >
+          {React.Children.toArray(children).length > 0 &&
             React.Children.toArray(children).filter(
-              (child) => !pokemonFilter || child.props.children.toLowerCase().startsWith(pokemonFilter)
-            )
-          )}
-
+              child =>
+                !pokemonFilter ||
+                child.props.children.toLowerCase().startsWith(pokemonFilter)
+            )}
         </ul>
       </div>
     )
@@ -71,25 +92,29 @@ export const PokemonsSelectList: React.FC<IPokemonsSelectList> = ({ userPokemons
   const CustomMenu = React.forwardRef(renderCustomMenu)
 
   return (
-     <Form.Group className="mb-3">
-          <Form.Label>{c('pokemons')}</Form.Label>
-          <Dropdown>
-            <Dropdown.Toggle as={CustomToggle} id="dropdown-custom-components">
-              {selectedPokemon?.name || t('select-a-pokemon')}
-            </Dropdown.Toggle>
+    <Form.Group className="mb-3">
+      <Form.Label>{c('pokemons')}</Form.Label>
+      <Dropdown>
+        <Dropdown.Toggle as={CustomToggle} id="dropdown-custom-components">
+          {selectedPokemon?.name || t('select-a-pokemon')}
+        </Dropdown.Toggle>
 
-            <Dropdown.Menu as={CustomMenu}>
-              {(pokemonsList && pokemonsList.length > 0) && pokemonsList.map(pokemon =>
-                <Dropdown.Item onClick={() => setSelectedPokemon(pokemon)}
-                  key={`list_pokemon_${pokemon.id}`}
-                  eventKey={`list_pokemon_${pokemon.id}`}
-                  className="d-flex align-items-center"
-                  active={pokemon.name === selectedPokemon?.name}>
-                  {pokemon.name}
-                </Dropdown.Item>
-              )}
-            </Dropdown.Menu>
-          </Dropdown>
-      </Form.Group>
+        <Dropdown.Menu as={CustomMenu}>
+          {pokemonsList &&
+            pokemonsList.length > 0 &&
+            pokemonsList.map(pokemon => (
+              <Dropdown.Item
+                onClick={() => setSelectedPokemon(pokemon)}
+                key={`list_pokemon_${pokemon.id}`}
+                eventKey={`list_pokemon_${pokemon.id}`}
+                className="d-flex align-items-center"
+                active={pokemon.name === selectedPokemon?.name}
+              >
+                {pokemon.name}
+              </Dropdown.Item>
+            ))}
+        </Dropdown.Menu>
+      </Dropdown>
+    </Form.Group>
   )
 }
