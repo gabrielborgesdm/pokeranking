afc57cec209424939499f71c19a867295cbc0c1d
Gabriel Borges <gabrielborgesdemoraes@gmail.com>
2021-09-25 21:44:06 -0300

    Implement Pokemon update

--- src/pages/api/_app/controllers/PokemonController.ts
+++ src/pages/api/_app/controllers/PokemonController.ts
@@ -1,14 +1,16 @@
 import fs from 'fs'
 import path from 'path'
 import { NextApiResponse } from 'next'
-import { ERROR, POKEMON_ALREADY_REGISTERED, POKEMON_NOT_FOUND, SUCCESS } from '../../../../configs/APIConfig'
+import { BEING_USED, ERROR, POKEMON_ALREADY_REGISTERED, POKEMON_NOT_FOUND, SUCCESS } from '../../../../configs/APIConfig'
 import { IRequest } from '../../../../configs/types/IRequest'
 import { abstractPokemon } from '../helpers/PokemonHelpers'
 import { sendResponse } from '../helpers/ResponseHelpers'
 import PokemonRepository from '../repositories/PokemonRepository'
 import { removeImage, writeImage } from '../helpers/ImageHelpers'
+import UserRepository from '../repositories/UserRepository'
 
 const pokemonRepository = new PokemonRepository()
+const userRepository = new UserRepository()
 
 export const getAllPokemons = async (req: IRequest, res: NextApiResponse) => {
   const pokemons = await pokemonRepository.getAll()
@@ -42,8 +44,32 @@ export const updatePokemon = async (req: IRequest, res: NextApiResponse) => {
   const { slug }: any = req.query
   const { pokemon: pokemonInfo } = req.body
   const pokemonId = parseInt(slug)
+
+  const pokemonFound = await pokemonRepository.getById(pokemonId)
+  if (!pokemonFound) return sendResponse(req, res, POKEMON_NOT_FOUND)
+
+  if (await pokemonRepository.get({ name: pokemonInfo.name })) {
+    return sendResponse(req, res, POKEMON_ALREADY_REGISTERED)
+  }
+
+  if (await userRepository.get({ pokemons: { $elemMatch: { pokemon: pokemonId } } })) {
+    return sendResponse(req, res, BEING_USED)
+  }
+
+  const imageName = `${pokemonInfo.name || pokemonFound.name}-${new Date().getTime()}.png`
+  if (pokemonInfo.image) {
+    if (writeImage(imageName, pokemonInfo.image)) {
+      removeImage(pokemonFound.image)
+    } else {
+      return sendResponse(req, res, ERROR)
+    }
+  }
+
+  pokemonInfo.image = imageName
+
   const pokemon = await pokemonRepository.update(pokemonId, pokemonInfo)
   if (!pokemon) {
+    removeImage(imageName)
     return sendResponse(req, res, ERROR)
   }
   return sendResponse(req, res, SUCCESS, { pokemon: abstractPokemon(pokemon) })
