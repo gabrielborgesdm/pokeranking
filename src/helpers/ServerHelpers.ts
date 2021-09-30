066c44eb91afd30fcb073856dd4754ab871b4e1d
Gabriel Borges <gabrielborgesdemoraes@gmail.com>
2021-09-29 21:58:04 -0300

    Implement integration with cloudinary

--- src/helpers/ServerHelpers.ts
+++
@@ -1,6 +0,0 @@
-import path from 'path'
-import { IRequest } from '../configs/types/IRequest'
-
-export const getImageURL = (req: IRequest, image: string) => {
-  return path.join('api', 'images', image)
-}
