72f238335283ef0063ce0c2a5bc2d1743816a00f
Gabriel Borges <gabrielborgesdemoraes@gmail.com>
2021-09-29 23:27:50 -0300

    Print cloudinary config

--- src/pages/api/_app/services/CloudinaryService.ts
+++ src/pages/api/_app/services/CloudinaryService.ts
@@ -1,10 +1,13 @@
 const cloudinary = require('cloudinary').v2
 
-cloudinary.config({
+const config = {
   cloud_name: process.env.CLOUDINARY_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
   secure: true
-})
+}
+
+console.log(config)
+cloudinary.config(config)
 
 export default cloudinary
