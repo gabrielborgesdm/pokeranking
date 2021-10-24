4a6cb467b0169327507a1a33b10cee7428baf5b3
Gabriel Borges <gabrielborgesdemoraes@gmail.com>
2021-10-24 13:05:03 -0300

    Add user paginated query, remove pokemons from get all

--- src/pages/api/_app/repositories/UserRepository.ts
+++ src/pages/api/_app/repositories/UserRepository.ts
@@ -1,64 +1,74 @@
-import { IUserAdd, IUserDocument } from "../../../../configs/types/IUser";
-import User from "../models/UserModel";
+import { IUserAdd, IUserDocument } from '../../../../configs/types/IUser'
+import User from '../models/UserModel'
 
 export default class UserRepository {
   async getAll(): Promise<Array<IUserDocument>> {
-    let users: Array<IUserDocument> = null;
+    let users: Array<IUserDocument> = null
     try {
-      users = await User.find().exec();
+      users = await User.find().exec()
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
-    return users;
+    return users
   }
 
   async getById(_id: string): Promise<IUserDocument> {
-    let user: IUserDocument = null;
+    let user: IUserDocument = null
     try {
-      user = await User.findById(_id).exec();
+      user = await User.findById(_id).exec()
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
-    return user;
+    return user
   }
 
   async get(query: object): Promise<IUserDocument> {
-    let user: IUserDocument = null;
+    let user: IUserDocument = null
     try {
-      user = await User.findOne(query).exec();
+      user = await User.findOne(query).exec()
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
-    return user;
+    return user
+  }
+
+  async getPaginated(pagination: any): Promise<Array<IUserDocument>> {
+    let users: Array<IUserDocument> = null
+    try {
+      users = await User.find().skip(pagination.skip).limit(pagination.limit).exec()
+    } catch (error) {
+      console.log(error)
+    }
+    return users
   }
 
   async delete(_id: string): Promise<IUserDocument> {
-    let user: IUserDocument = null;
+    let user: IUserDocument = null
     try {
-      user = await User.findByIdAndDelete(_id).exec();
+      user = await User.findByIdAndDelete(_id).exec()
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
-    return user;
+    return user
   }
 
   async store(userInfo: IUserAdd): Promise<IUserDocument> {
-    let user: IUserDocument = null;
+    let user: IUserDocument = null
     try {
-      user = await User.create(userInfo);
+      user = await User.create(userInfo)
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
-    return user;
+    return user
   }
 
   async update(_id: string, userInfo: any): Promise<IUserDocument> {
-    let user: any = null;
+    let user: any = null
     try {
-      user = await User.findByIdAndUpdate(_id, userInfo, { new: true });
+      user = await User.findByIdAndUpdate(_id, userInfo, { new: true })
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
-    return user;
+    return user
   }
 }
