6e6a9e4a9a9bfc4c624aa8ee0992d385eb0e1054
Gabriel Borges <gabrielborgesdemoraes@gmail.com>
2022-01-30 18:25:20 -0300

    Lint more files

--- src/pages/api/_app/helpers/AuthenticationHelpers.ts
+++ src/pages/api/_app/helpers/AuthenticationHelpers.ts
@@ -1,31 +1,31 @@
-import jwt, { JwtPayload } from "jsonwebtoken";
-import bcrypt from "bcrypt";
+import jwt, { JwtPayload } from 'jsonwebtoken'
+import bcrypt from 'bcrypt'
 
-const accessTokenSecret = String(process.env.ACCESS_TOKEN_SECRET);
+const accessTokenSecret = String(process.env.ACCESS_TOKEN_SECRET)
 
 export interface Payload extends JwtPayload {
   _id: string;
 }
 
 export const hashPassword = async (password: string) =>
-  await bcrypt.hash(password, 10);
+  await bcrypt.hash(password, 10)
 
 export const isPasswordValid = async (
   reqPassword: string,
   accountHash: string
 ) => {
-  return await bcrypt.compare(reqPassword, accountHash);
-};
+  return await bcrypt.compare(reqPassword, accountHash)
+}
 
 export const generateAccessToken = (payload: object) =>
-  jwt.sign(payload, accessTokenSecret);
+  jwt.sign(payload, accessTokenSecret)
 
 export const verifyTokenAndGetUserId = (token: string): string => {
-  let payload: JwtPayload | null;
+  let payload: JwtPayload | null
   try {
-    payload = jwt.verify(token, accessTokenSecret) as Payload;
+    payload = jwt.verify(token, accessTokenSecret) as Payload
   } catch (error) {
-    console.log(error);
+    console.log(error)
   }
-  return payload?._id ? payload._id : null;
-};
+  return payload?._id ? payload._id : null
+}
--- src/pages/api/_app/middlewares/AuthenticationMiddleware.ts
+++ src/pages/api/_app/middlewares/AuthenticationMiddleware.ts
@@ -1,42 +1,42 @@
-import { NextApiResponse } from "next";
-import { FORBIDDEN, UNAUTHORIZED } from "../../../../configs/APIConfig";
-import { IRequest } from "../../../../configs/types/IRequest";
-import { verifyTokenAndGetUserId } from "../helpers/AuthenticationHelpers";
-import { sendResponse } from "../helpers/ResponseHelpers";
-import UserRepository from "../repositories/UserRepository";
+import { NextApiResponse } from 'next'
+import { FORBIDDEN, UNAUTHORIZED } from '../../../../configs/APIConfig'
+import { IRequest } from '../../../../configs/types/IRequest'
+import { verifyTokenAndGetUserId } from '../helpers/AuthenticationHelpers'
+import { sendResponse } from '../helpers/ResponseHelpers'
+import UserRepository from '../repositories/UserRepository'
 
-const userRepository = new UserRepository();
+const userRepository = new UserRepository()
 
 const AuthenticationMiddleware = async (
   req: IRequest,
   res: NextApiResponse
 ): Promise<boolean> => {
-  let isOkay = true;
-  const token = getTokenFromRequest(req);
+  let isOkay = true
+  const token = getTokenFromRequest(req)
   if (!token) {
-    sendResponse(req, res, UNAUTHORIZED);
-    isOkay = false;
+    sendResponse(req, res, UNAUTHORIZED)
+    isOkay = false
   } else {
-    const _id = verifyTokenAndGetUserId(token);
+    const _id = verifyTokenAndGetUserId(token)
     if (!_id || !(await validateAndAddUserToRequest(_id, req))) {
-      sendResponse(req, res, FORBIDDEN);
-      isOkay = false;
+      sendResponse(req, res, FORBIDDEN)
+      isOkay = false
     }
   }
-  return isOkay;
-};
+  return isOkay
+}
 
 const getTokenFromRequest = (req: IRequest): string => {
-  const authHeader = req.headers.authorization;
-  const token = authHeader && authHeader.split(" ")[1];
-  return token;
-};
+  const authHeader = req.headers.authorization
+  const token = authHeader && authHeader.split(' ')[1]
+  return token
+}
 
 const validateAndAddUserToRequest = async (_id: string, req: IRequest) => {
-  const response = await userRepository.getById(_id);
-  if (!response) return false;
-  req.user = response.toObject();
-  return true;
-};
+  const response = await userRepository.getById(_id)
+  if (!response) return false
+  req.user = response.toObject()
+  return true
+}
 
-export default AuthenticationMiddleware;
+export default AuthenticationMiddleware
