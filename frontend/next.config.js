08a7159f7938ec21da8e4256a4f350186592b6b5
Gabriel Borges de Moraes <31298094+gabrielborgesdm@users.noreply.github.com>
2023-04-11 22:16:22 -0300

    Create next.config.js

---
+++ frontend/next.config.js
@@ -0,0 +1,20 @@
+const withPlugins = require('next-compose-plugins')
+const nextTranslate = require('next-translate')
+
+const nextConfig = {
+  esModule: true,
+  reactStrictMode: true,
+  typescript: {
+    ignoreBuildErrors: true
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true
+  },
+  serverRuntimeConfig: {
+    PROJECT_ROOT: `${__dirname}/frontend`
+  }
+}
+
+module.exports = withPlugins([nextTranslate], nextConfig)
