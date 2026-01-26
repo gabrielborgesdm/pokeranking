1546e4e3106de471f1c5fc1707054da0a5b4f1cb
Gabriel Borges <gabrielborgesdemoraes@gmail.com>
2026-01-26 10:36:49 -0300

    feat: add sentry to the frontend and enable logs on backend

---
+++ frontend/sentry.server.config.ts
@@ -0,0 +1,16 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://e7e685e550342c5b405340dc46bf3dff@o1000498.ingest.us.sentry.io/4510776955764736",
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+});
